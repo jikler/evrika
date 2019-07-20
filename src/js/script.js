02c488bbb480acb11dc5931acb13c93657d45ec7
dorokhovgold@gmail.com <dorokhovgold@gmail.com>
2019-07-21 00:16:37 +0300

    Доделал блок calc (без адаптива)

--- src/js/script.js
+++ src/js/script.js
@@ -9,20 +9,20 @@ $(document).ready(function(){
 
 // form calc start
 $(function() {
-  var uah_min = $("#user_age_handler_min");
-  var uah_max = $("#user_age_handler_max");
-$("#user_age").slider({
+  var uah_min = $("#user_filter_handler_min");
+  var uah_max = $("#user_filter_handler_max");
+$("#user_filter").slider({
   range: true,
   min: 1,
   max: 69,
   values: [1, 69],
   create: function() {
-   uah_min.text(1);
-    uah_max.text(69);
+   uah_min.text('от 1');
+    uah_max.text('до 69');
   },
   slide: function(event, ui) {
-    $("#user_age_min").val(ui.values[0]);
-    $("#user_age_max").val(ui.values[1]);
+    $("#user_filter_min").val(ui.values[0]);
+    $("#user_filter_max").val(ui.values[1]);
     uah_min.text( ui.values[0] );
     uah_max.text( ui.values[1] );
   }
