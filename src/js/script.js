388703760f2047423b9d29c78b0df3fadbc0b64b
dorokhovgold@gmail.com <dorokhovgold@gmail.com>
2019-07-21 16:49:53 +0300

    Релиз

--- src/js/script.js
+++ src/js/script.js
@@ -29,3 +29,22 @@ $("#user_filter").slider({
 });
 });
 // form calc end
+
+// color svg start
+$('img.img-svg').each(function(){
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function(data) {
+    var $svg = $(data).find('svg');
+    if(typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+// color svg end
